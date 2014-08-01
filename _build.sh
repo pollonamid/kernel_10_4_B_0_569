85de8115f3dd4381ecf36eeabd814dd3d48e542d
Adrian Ulrich <adrian@blinkenlights.ch>
2014-08-01 09:44:42 +0200

    copy radio-iris-transport

--- _build.sh
+++ _build.sh
@@ -7,5 +7,8 @@ make ARCH=arm CROSS_COMPILE=$TCHAIN -j 8
 
 # add new kernel
 cp arch/arm/boot/zImage ../../../device/sony/c6603/kernel 
-# overwrite wlan driver of sony
+
+# drop kernel modules
 cp ./drivers/staging/prima/wlan.ko  ../../../vendor/sony/yuga_blobs/system/lib/modules/wlan.ko
+cp ./drivers/media/radio/radio-iris-transport.ko ../../../vendor/sony/yuga_blobs/system/lib/modules/radio-iris-transport.ko
+
