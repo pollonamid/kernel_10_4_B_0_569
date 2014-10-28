384281d473feabc5529b9dfce9601af2d4b2cc9f
Adrian Ulrich <adrian@blinkenlights.ch>
2014-10-28 22:00:15 +0100

    refresh headers after build finished

--- _build.sh
+++ _build.sh
@@ -9,3 +9,6 @@ make ARCH=arm CROSS_COMPILE=$TCHAIN -j 8
 cp arch/arm/boot/zImage ../../../device/sony/c6603/kernel 
 # overwrite wlan driver of sony
 cp ./drivers/staging/prima/wlan.ko  ../../../vendor/sony/yuga_blobs/system/lib/modules/wlan.ko
+
+# refresh kernel headers
+( cd ../../../device/sony/lagan/ && sh update_kernel_headers.sh )
